17523409d29b70fcc3030fd5dda3bac842421cf7
shreya <goyalshreya317@gmail.com>
2020-03-07 00:54:50 -0500

    Shreya's database

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('personalweb/', include('personalweb.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
